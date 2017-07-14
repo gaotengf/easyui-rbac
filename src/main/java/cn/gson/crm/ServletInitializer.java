3e5fe62a7a4538c3f0cb979ced7ba943c39d446a
____′↘夏悸 <hua3651101@126.com>
2017-07-14 16:41:21 +0800

    补充注释

--- src/main/java/cn/gson/crm/ServletInitializer.java
+++ src/main/java/cn/gson/crm/ServletInitializer.java
@@ -11,7 +11,7 @@ import javax.servlet.ServletContext;
  * <p>****************************************************************************</p>
  * <p><b>Copyright © 2010-2017 soho team All Rights Reserved<b></p>
  * <ul style="margin:15px;">
- * <li>Description : cn.gson.crm</li>
+ * <li>Description : 如果是要打成war包，放到tomcat之类的web服务器运行，必须有此类</li>
  * <li>Version     : 1.0</li>
  * <li>Creation    : 2017年07月05日</li>
  * <li>Author      : 郭华</li>
